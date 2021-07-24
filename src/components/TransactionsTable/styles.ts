6841d961cff0a2efa79ff4e735ab46ac00583f55
Israel Fagundes <israel.fagundes2001@gmail.com>
2021-07-23 22:02:21 -0300

    fix: :lipstick: Fix border-radius value.

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -20,7 +20,7 @@ export const Container = styled.div`
       border: 0;
       background: var(--shape);
       color: var(--text-body);
-      border-radius: 0%.25rem;
+      border-radius: 0.25rem;
 
       &:first-child {
         color: var(--text-title);
